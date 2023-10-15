78115390cace1c3d8c898428a09db968f69151d9
XMH <XMH@DESKTOP-ENIFHGH>
2023-10-15 22:19:21 +0700

    remove file

--- PortForwardServer/Dto/HandleIncomingConnectionInputDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.SignalR;
-
-namespace PortForwardServer.Dto
-{
-    public class HandleIncomingConnectionStateDto
-    {
-        public IHubCallerClients<ISocketServerHub>? Clients { get; set; }
-    }
-}
